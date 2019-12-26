612121ce4a0d0603cbf5ed252444790ceb48ec9e
RoccoDev <roccodev.business@gmail.com>
2019-12-26 23:58:07 +0100

    Improved write check

--- src/check/write.rs
+++ src/check/write.rs
@@ -7,7 +7,7 @@ pub struct WriteChecker;
 impl Checker for WriteChecker {
     fn check(&self, instr: Opcode, block_a: &str, block_b: &str) -> CheckResult {
         err_if!(instr == Opcode::WriteShort && !block_b.starts_with("0000"), errors::WRONG_SIZE);
-        err_if!(instr == Opcode::WriteByte && !block_a.starts_with("000000"), errors::WRONG_SIZE);
+        err_if!(instr == Opcode::WriteByte && !block_b.starts_with("000000"), errors::WRONG_SIZE);
         CheckResult::Pass
     }
 }
